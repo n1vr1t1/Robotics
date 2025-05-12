20c5af4bb9b5af843c1cf6bf16640d4fe196cf46
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-05-12 12:34:22 +0200

    Update startup.sh

--- ros2_ws/startup.sh
+++ ros2_ws/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ZIP_FILE="external/libtorch.zip"  
-DESTINATION_DIR="external/libtorch"  #
+DESTINATION_DIR="external"  #
 TORCH_URL="https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.2.2%2Bcpu.zip" 
 
-if [ ! -d "external" ]; then
+if [ ! -d "$DESTINATION_DIR" ]; then
     echo "Creating external directory"
-    mkdir external
+    mkdir "$DESTINATION_DIR"
 fi
 
 if [ ! -f "$ZIP_FILE" ]; then
