b722ca5d98d82e69dbc44070bf8b821031dcbcad
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-05-13 15:08:43 +0200

    Update compute_path_service.cpp

--- ros2_ws/src/motion/src/compute_path_service.cpp
+++ ros2_ws/src/motion/src/compute_path_service.cpp
@@ -3,41 +3,38 @@
 namespace motion{
     
     PathPlannerNode::PathPlannerNode() : Node("path_planner_node") {
-        // Initialize service and publisher
-        initialize_service();
-        initialize_publisher();
+        //initialize service
+        
+        // Defining variable callback
+        auto service_callback = [this](const std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Request> request,
+                                        std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Response> response){
+                                        this->handle_compute_path(request, response);};
+
+        // Creating the service using the callback
+        service_ = this->create_service<custom_msg_interfaces::srv::ComputePath>(
+            "compute_path",
+            service_callback
+        
+        //initialize_publisher
+        auto qos = rclcpp::QoS(10).transient_local().reliable();
+        publisher_ = this->create_publisher<geometry_msgs::msg::PoseArray>("computed_trajectory", qos);
+            
         RCLCPP_INFO(this->get_logger(), "Path Planner Node is ready");
     }
     
-    void PathPlannerNode::initialize_service() {
-        service_ = this->create_service<custom_msg_interfaces::srv::ComputePath>(
-            "compute_path",
-            [this](const std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Request> request,
-                   std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Response> response) {
-                handle_compute_path(request, response);
-            }
-        );
-    }
-    
-    void PathPlannerNode::initialize_publisher() {
-        publisher_ = this->create_publisher<geometry_msgs::msg::PoseArray>(
-            "computed_trajectory",
-            rclcpp::QoS(10).transient_local().reliable()
-        );
-    }
     
     void PathPlannerNode::handle_compute_path(
         const std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Request> request,
         std::shared_ptr<custom_msg_interfaces::srv::ComputePath::Response> response) {
         
         // Perform interpolation
-        auto interpolated_poses = compute_interpolated_poses(
+        auto interpolated_poses = compute_interpolated_poses(    //40
             request->pose_start, request->pose_end, request->num_interpolations
         );
         response->poses = interpolated_poses;
     
         // Publish for visualization
-        publish_pose_array(interpolated_poses);
+        publish_pose_array(interpolated_poses);    //70
     }
     
     std::vector<geometry_msgs::msg::Pose> PathPlannerNode::compute_interpolated_poses(
@@ -52,22 +49,31 @@ namespace motion{
                                end_pose.orientation.z, end_pose.orientation.w);
     
         for (int i = 0; i <= num_interpolations; ++i) {
-            double t = static_cast<double>(i) / num_interpolations;
-    
-            geometry_msgs::msg::Pose interp_pose;
-            interp_pose.position.x = lerp(start_pose.position.x, end_pose.position.x, t);
-            interp_pose.position.y = lerp(start_pose.position.y, end_pose.position.y, t);
-            interp_pose.position.z = lerp(start_pose.position.z, end_pose.position.z, t);
-    
-            tf2::Quaternion q_interp = q_start.slerp(q_end, t);
-            interp_pose.orientation.x = q_interp.x();
-            interp_pose.orientation.y = q_interp.y();
-            interp_pose.orientation.z = q_interp.z();
-            interp_pose.orientation.w = q_interp.w();
-    
-            poses.push_back(interp_pose);
+            double t = static_cast<double>(i) / num_interpolations;            
+            poses.push_back(s_lerp(start_pose, end_pose, t););
         }
         return poses;
+    }
+
+    geometry_msgs::msg::Pose PathPlannerNode::s_lerp(const geometry_msgs::msg::Point& start, const geometry_msgs::msg::Point& end, double t) const {
+        
+        geometry_msgs::msg::Pose result;
+    
+        // Linear interpolation of position
+        result.position.x = start.position.x + t * (end.position.x - start.position.x);
+        result.position.y = start.position.y + t * (end.position.y - start.position.y);
+        result.position.z = start.position.z + t * (end.position.z - start.position.z);
+    
+        // Spherical linear interpolation (SLERP) of orientation
+        tf2::Quaternion q_start, q_end;
+        tf2::fromMsg(start.orientation, q_start);
+        tf2::fromMsg(end.orientation, q_end);
+    
+        tf2::Quaternion q_interp = q_start.slerp(q_end, t);
+        result.orientation = tf2::toMsg(q_interp);
+    
+        return result;
+        
     }
     
     void PathPlannerNode::publish_pose_array(const std::vector<geometry_msgs::msg::Pose>& poses) const {
@@ -78,9 +84,7 @@ namespace motion{
         publisher_->publish(pose_array);
     }
     
-    double PathPlannerNode::lerp(double a, double b, double t) const {
-        return a + t * (b - a);
-    }
+    
 }
 
 int main(int argc, char** argv) {
