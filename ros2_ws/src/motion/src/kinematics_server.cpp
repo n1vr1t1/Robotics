cb212e8502ece6c0cbf2a15861364e6e08267536
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-05-13 12:50:32 +0200

    Update and rename direct_kin_server.cpp to kinematics_server.cpp

--- ros2_ws/src/motion/src/kinematics_server.cpp
+++ ros2_ws/src/motion/src/kinematics_server.cpp
@@ -433,7 +433,7 @@ namespace motion
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<DirectKinServer>();
+    auto node = std::make_shared<motion::DirectKinServer>();
     rclcpp::spin(std::make_shared<motion::InverseKinServer>());
     rclcpp::spin(node);
     rclcpp::shutdown();
