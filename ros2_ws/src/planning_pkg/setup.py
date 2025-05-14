78764618b4297fa7c0e6bfb5465e10da8b6ac23b
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-05-14 18:58:16 +0200

    Update setup.py

--- ros2_ws/src/planning_pkg/setup.py
+++ ros2_ws/src/planning_pkg/setup.py
@@ -6,7 +6,7 @@ package_name = 'planning_pkg'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
