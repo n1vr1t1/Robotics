d3242db76be16f69e03423c8c51f320acc4132e1
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-05-15 18:18:35 +0200

    Update kinematics_server.cpp

--- ros2_ws/src/motion/src/kinematics_server.cpp
+++ ros2_ws/src/motion/src/kinematics_server.cpp
@@ -102,10 +102,10 @@ namespace motion
     void InverseKinServer::computeIKCallback(const std::shared_ptr<custom_msg_interfaces::srv::ComputeIK::Request> request,
                                              std::shared_ptr<custom_msg_interfaces::srv::ComputeIK::Response> response){
      // Extract the target pose from the request
-     auto & pose = request->target_pose;
+     //auto & pose = request->target_pose;
 
-     auto position = & request->target_pose.position;
-     auto orientation =  & request->target_pose.orientation;
+     auto position = request->target_pose.position;
+     auto orientation =  request->target_pose.orientation;
     
      // Convert quaternion to Eigen rotation matrix (using double precision then cast to float)
      Eigen::Quaterniond q_eig(orientation.x, orientation.y, orientation.z, orientation.w);
