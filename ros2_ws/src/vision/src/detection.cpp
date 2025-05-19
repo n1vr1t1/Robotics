a2d42de56de4fbf91deced264c8b7bcd9300fa59
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-05-19 14:37:06 +0200

    changing path of model

--- ros2_ws/src/vision/src/detection.cpp
+++ ros2_ws/src/vision/src/detection.cpp
@@ -19,7 +19,7 @@ class DetectionNode : public rclcpp::Node {
 public:
     DetectionNode() : Node("yolo_detection_node") {
         try {
-            model = torch::jit::load("/home/ubuntu/ros2_ws/src/Robotics/ros2_ws/src/vision/model/yolo11n_0dropout.pt");
+            model = torch::jit::load("/home/ubuntu/ros2_ws/src/Robotics/ros2_ws/src/vision/model/best.torchscript");
             model.to(torch::kCPU);
             model.eval();
         } catch (const c10::Error &e) {
