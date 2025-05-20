1bad115be80e8bf3a1c45395f4fcbbc256e88314
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-05-20 11:00:54 +0200

    Update detection.cpp

--- ros2_ws/src/vision/src/detection.cpp
+++ ros2_ws/src/vision/src/detection.cpp
@@ -115,7 +115,7 @@ private:
             auto max_result = class_scores.max(0);
             float class_conf = std::get<0>(max_result).item<float>(); //finding highest confidence
 
-            if(obj_conf > 0.1) RCLCPP_INFO(this->get_logger(), "Confidence is:%f(obj) %f(class)", obj_conf, class_conf);
+            if(obj_conf > 0.1 || class_conf > 0.1) RCLCPP_INFO(this->get_logger(), "Confidence is:%f(obj) %f(class)", obj_conf, class_conf);
             
             
             float final_conf = obj_conf * class_conf;
