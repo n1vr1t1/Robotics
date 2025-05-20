db2ec38801eb714e68fdccc36186789eb475fd03
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-05-20 12:46:19 +0200

    Update detection.cpp

--- ros2_ws/src/vision/src/detection.cpp
+++ ros2_ws/src/vision/src/detection.cpp
@@ -122,6 +122,7 @@ private:
             for(int j=0;j<15;i++){
                 RCLCPP_INFO(this->get_logger(), "%d:%f", j, pred[j].item<float>());
             }
+            subscription.reset();
             return;
             float obj_conf = pred[4].item<float>();
 
