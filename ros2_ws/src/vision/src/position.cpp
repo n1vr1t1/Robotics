d9c69aa8090026cd91bc8f560650971bdedb6d5a
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-05-29 13:19:09 +0200

    debuging denormalization thing

--- ros2_ws/src/vision/src/position.cpp
+++ ros2_ws/src/vision/src/position.cpp
@@ -64,8 +64,8 @@ class CameraPoseNode : public rclcpp::Node{
             
             for(size_t i =0; i+2 < positions.size(); i+=3){
                 int id = static_cast<int>(positions[i]);
-                int u = static_cast<int>(positions[i+1] * width);
-                int v = static_cast<int>(positions[i+2] * height);
+                int u = static_cast<int>(positions[i+1])* width;
+                int v = static_cast<int>(positions[i+2])* height;
                 int index = v * width + u;
 
                 sensor_msgs::PointCloud2ConstIterator<float> iter_x(*current_cloud, "x");
