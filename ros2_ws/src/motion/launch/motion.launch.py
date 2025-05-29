c04f890d69b9dc4c5ce592e6c26b6f76444455e2
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-05-29 11:26:21 +0200

    Update motion.launch.py

--- ros2_ws/src/motion/launch/motion.launch.py
+++ ros2_ws/src/motion/launch/motion.launch.py
@@ -8,27 +8,27 @@ def generate_launch_description():
             executable='inverse_kin_server_node',
             name='inverse_kin_server',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            #parameters=[{'use_sim_time': False}]
         ),
         launch_ros.actions.Node(
             package='motion',
             executable='direct_kin_server',
             name='direct_kin_server',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            #parameters=[{'use_sim_time': False}]
         ),
         launch_ros.actions.Node(
             package='motion',
             executable='compute_trajectory_service',
             name='compute_trajectory_service',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            #parameters=[{'use_sim_time': False}]
         ),
         launch_ros.actions.Node(
             package='motion',
             executable='compute_path_service',
             name='compute_path_service',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            #parameters=[{'use_sim_time': False}]
         ),
     ])
