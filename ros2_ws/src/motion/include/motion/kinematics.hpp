229f62d5d50187b02c8a81e659edb744f5eb13bb
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-06-06 23:20:15 +0200

    Update and rename kinematics_server.hpp to kinematics.hpp

--- ros2_ws/src/motion/include/motion/kinematics.hpp
+++ ros2_ws/src/motion/include/motion/kinematics.hpp
@@ -26,12 +26,10 @@ namespace motion
         DirectKinServer();
     
     private:
-        // Service callback function
         void computeDirectKinematics(
             const std::shared_ptr<custom_msg_interfaces::srv::ComputeDirKin::Request> request,
             std::shared_ptr<custom_msg_interfaces::srv::ComputeDirKin::Response> response);
     
-        // Utility functions
         Eigen::Matrix4d Tij(double th, double alpha, double d, double a);
         std::shared_ptr<custom_msg_interfaces::srv::ComputeDirKin::Response> ur5Direct(const std::vector<double>& Th, double scaleFactor, Eigen::Vector3d& pe,
                                     Eigen::Matrix3d& Re, std::vector<Eigen::Matrix4d>& Tm);
@@ -45,28 +43,23 @@ namespace motion
       explicit InverseKinServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
       
     private:
-      // Callback for the "ComputeIK" service
       void computeIKCallback(
         const std::shared_ptr<custom_msg_interfaces::srv::ComputeIK::Request> request,
         std::shared_ptr<custom_msg_interfaces::srv::ComputeIK::Response>      response);
     
-      // Our UR5 IK solver function adapted from your code
-      // (uses single-precision floats internally, but we will convert to double in the response)
       Eigen::MatrixXd ur5Inverse(
         const Eigen::Vector3f & p60, 
         const Eigen::Matrix3f & R60, 
         float scaleFactor
       );
     
-      // A small helper function to check for near-zero float values
-      inline bool almzero(float x) const
-      {
+      inline bool almzero(float x) const{      {
         return std::fabs(x) < 1e-7;
       }
     
     private:
       rclcpp::Service<custom_msg_interfaces::srv::ComputeIK>::SharedPtr service_;
     };
-}  // namespace motion
+}  
 
 
