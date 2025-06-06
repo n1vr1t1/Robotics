cc872608a543acd1fcc36d081fdce5c2472e5b98
SaraFrancavilla <134162794+SaraFrancavilla@users.noreply.github.com>
2025-06-07 01:06:42 +0200

    Update motion.launch.py

--- ros2_ws/src/motion/launch/motion.launch.py
+++ ros2_ws/src/motion/launch/motion.launch.py
@@ -5,27 +5,31 @@ def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='motion',
-            executable='inverse_kin_server_node',
-            name='inverse_kin_server',
+            executable='kinematics_node',
+            name='kinematics_node',
             output='screen',
             parameters=[{'use_sim_time': True}]
-            #parameters=[{'use_sim_time': False}]
         ),
-        launch_ros.actions.Node(
-            package='motion',
-            executable='direct_kin_server',
-            name='direct_kin_server',
-            output='screen',
-            parameters=[{'use_sim_time': True}]
-            #parameters=[{'use_sim_time': False}]
-        ),
+        #launch_ros.actions.Node(
+        #    package='motion',
+        #    executable='inverse_kin_server_node',
+        #    name='inverse_kin_server',
+        #    output='screen',
+        #    parameters=[{'use_sim_time': True}]
+        #),
+        #launch_ros.actions.Node(
+        #    package='motion',
+        #    executable='direct_kin_server',
+        #    name='direct_kin_server',
+        #    output='screen',
+        #    parameters=[{'use_sim_time': True}]
+        #),
         launch_ros.actions.Node(
             package='motion',
             executable='compute_trajectory_service',
             name='compute_trajectory_service',
             output='screen',
             parameters=[{'use_sim_time': True}]
-            #parameters=[{'use_sim_time': False}]
         ),
         launch_ros.actions.Node(
             package='motion',
@@ -33,6 +37,5 @@ def generate_launch_description():
             name='compute_path_service',
             output='screen',
             parameters=[{'use_sim_time': True}]
-            #parameters=[{'use_sim_time': False}]
         ),
     ])
